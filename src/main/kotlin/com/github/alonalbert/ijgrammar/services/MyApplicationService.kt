c734da421f54a33b411dcfcd4240d590ec44def9
GitHub Action <action@github.com>
2021-10-29 20:35:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/alonalbert/ijgrammar/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alonalbert/ijgrammar/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alonalbert.ijgrammar.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alonalbert.ijgrammar.MyBundle
 
 class MyApplicationService {
 
