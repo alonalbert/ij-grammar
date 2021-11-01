8cd96310151ddb14f60b0207c64cbb64b277d77f
Alon Albert <aalbert@google.com>
2021-11-01 10:36:45 -0700

    Test

--- src/test/kotlin/com/github/alonalbert/ijgrammar/MyPluginTest.kt
+++ src/test/kotlin/com/github/alonalbert/ijgrammar/MyPluginTest.kt
@@ -1,31 +1,31 @@
 package com.github.alonalbert.ijgrammar
 
+import com.github.alonalbert.ijgrammar.simple.SimpleFileType
+import com.github.alonalbert.ijgrammar.simple.SimpleLanguage
+import com.github.alonalbert.ijgrammar.simple.SimpleParserDefinition
 import com.intellij.ide.highlighter.XmlFileType
+import com.intellij.lang.LanguageParserDefinitions
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.util.hasErrorElementInRange
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
 
+private val simpleParserDefinition = SimpleParserDefinition()
+
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+  fun testName() {
+    val text = "foo: bar"
+    val psiFile = parse(text)
+    println(psiFile.firstChild)
+  }
 
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
+  @Suppress("SameParameterValue")
+  private fun parse(text: String): PsiFile =
+    PsiFileFactory.getInstance(project).createFileFromText("temp.simple", SimpleFileType.INSTANCE, text)
 }
